6aa54dff739f6d6a72b872a0270578c3fb5d3bf1
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-21 00:04:35 +0530

    added models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django_mongodb_backend.fields import ObjectIdAutoField
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+class CustomUser(models.Model):
+    id = ObjectIdAutoField(primary_key=True)    
+    role = models.CharField(max_length=10, default='store_admin')
+    contact = models.IntegerField(max_length=12)
+
+class Store(models.Model):
+    id = ObjectIdAutoField(primary_key=True)
+    location = models.CharField(max_length=100, default='Indore')
+    revenue = models.FloatField(max_length=150, null=True)
+    customers = models.IntegerField(max_length=100, null=True)
+    employees = models.IntegerField(max_length=100)
+    rating = models.FloatField(default=5)
+
+class Tickets(models.Model):
+    id = ObjectIdAutoField(primary_key=True)
+    category = models.CharField(max_length=100)
+    isResolved = models.CharField(max_length=10)
+    date = models.DateTimeField(default=timezone.now())
+    priority = models.CharField(max_length=50)
+
+class Product(models.Model):
+    id = ObjectIdAutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_url = models.CharField(max_length=100)
+    sentiment = models.CharField(max_length=100)
+    review = models.CharField(max_length=150)
+    reviewer_name = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now())
+
+
+
+
