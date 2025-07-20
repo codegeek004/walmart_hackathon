9e6c81768dcea0c386a17f896991d6f5053a8275
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-21 00:30:29 +0530

    added first endpoint index of api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import View
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
 
-# Create your views here.
+user = get_user_model()
+
+@api_view(['GET'])
+def index_view(request):
+    if request.method == 'GET':
+        return Response({"message" : "Welcome to Dashboard"})
+
+
+
