0d6573fd026f007a3136e4719d9c38b1ef30b5bf
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-23 11:57:14 +0530

    added url endpoint for dashboard view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-        path('', index_view, name='index_view')
+        path('', index_view, name='index_view'),
+        path('dashboard/', Dashboard.as_view(), name='dashboard'),
         ]
