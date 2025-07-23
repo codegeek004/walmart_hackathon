1a208d13f8d379fc0adcaa479d51a2fcd7372147
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-23 11:56:41 +0530

    added dashboard view which returns response total feedback, avg rating, critical issues

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,13 @@ from rest_framework.views import APIView
 from .models import *
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
+from pymongo import MongoClient
+
+try:
+    client = MongoClient('mongodb://root:root@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.3')
+    db = client['walmart']
+except Exception as e:
+    print(f'Exception is {e}')
 
 user = get_user_model()
 
@@ -12,6 +19,31 @@ user = get_user_model()
 def index_view(request):
     if request.method == 'GET':
         return Response({"message" : "Welcome to Dashboard"})
+
+class Dashboard(APIView):
+    def get(self, request, *args, **kwargs):
+        total_feedbacks = Product.objects.count()
+        
+        products = db['api_product']
+        products_objects = products.find({}, {"_id":0, "Rating":1})
+        ratings = []
+        for i in products_objects:
+            rating = i["Rating"]
+            ratings.append(int(rating))
+        
+        sentiment_qs = Product.objects.filter(sentiment="positive").count()
+        critical_issues = Tickets.objects.filter(category='critical').count()
+        avg_rating = sum(ratings)/len(ratings)
+     
+        return Response({
+                "Total Feedbacks" : total_feedbacks,
+                "Sentiments" : sentiment_qs,
+                "Critical Issues" : critical_issues,
+                "Average Rating" : avg_rating
+            })
+
+
+
 
 
 
