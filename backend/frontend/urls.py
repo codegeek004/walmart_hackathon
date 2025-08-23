cb8261de48b31547df16c7f26394deceb8396890
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-24 00:25:33 +0530

    added urls for home page and dashboard page

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
-urlpatterns = []
+from .views import *
+urlpatterns = [
+		path('', home_page_view, name='home_page_view'),
+		path('dashboard/', dashboard_view, name='dashboard_view')
+]
