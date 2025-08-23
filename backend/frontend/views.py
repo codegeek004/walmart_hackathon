df4b84e20e44748e297ee2d7b6cd0fbd28371868
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-24 00:26:56 +0530

    added new home_page_view and dashboard_view to call the api endpoints which will render jinja templates

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 import requests
 
-dashboard_url = 'http://127.0.0.1:8000/dashboard'
+home_page_url = 'http://127.0.0.1:8080/api/'
+def home_page_view(request):
+	response = requests.get(home_page_url).json()
+	return render(request, 'home.html', response)
+
+dashboard_url = 'http://127.0.0.1:8080/api/dashboard/'
 def dashboard_view(request):
-	return render(request, 'dashboard.html')
+	response = requests.get(dashboard_url).json()
+	print(response)
+	return render(request, 'dashboard.html', response)
