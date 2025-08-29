e0dfeae52004cebff5eea7d52fe287ba3721e83b
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-29 22:20:55 +0530

    added Feedback view to display the info about the product and generate insights from the data

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from pymongo import MongoClient
 from django.db.models import Count, Q
+from .serializers import *
 
 try:
     client = MongoClient('mongodb://root:root@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.3')
@@ -24,14 +25,14 @@ def index_view(request):
 class Dashboard(APIView):
     def get(self, request, *args, **kwargs):
         total_feedbacks = Product.objects.count()
-        
         products = db['api_product']
-        products_objects = products.find({}, {"_id":0, "Rating":1})
+        products_objects = products.find({}, {"_id":0, "rating":1})
         ratings = []
         for i in products_objects:
-            rating = i["Rating"]
-            ratings.append(int(rating))
-        
+            rating = i.get('rating')
+            if rating is not None:
+                ratings.append(int(rating))
+
         positive_sentiment_qs = Product.objects.filter(sentiment="positive").count()
         tickets_qs = Tickets.objects.aggregate(
                 active_tickets=Count('id', filter=Q(isResolved=False)),
@@ -48,9 +49,13 @@ class Dashboard(APIView):
                 "average_rating" : avg_rating
             })
 
+class Feedback(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = FeedbackSerializer(products, many=True)
+        return Response(serializer.data)
 
-
-
+    
 
 
 
