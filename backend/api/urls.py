32696a0d7399dd418081e85015798f3a91e5810a
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-31 23:08:38 +0530

    added feedback url, tickets url and patch tickets url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,7 @@ from .views import *
 urlpatterns = [
         path('', index_view, name='index_view'),
         path('dashboard/', Dashboard.as_view(), name='dashboard'),
-        path('feedback/', Feedback.as_view(), name='feedback')
+        path('feedback/', Feedback.as_view(), name='feedback'),
+        path('tickets/', Tickets.as_view(), name='tickets'),
+        path('tickets/<str:ticket_id>/', Tickets.as_view(), name='ticket_status_update')
         ]
